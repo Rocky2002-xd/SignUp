02f8a35d5840f13e96923ab1615bf386c9ab425b
Guru Raghav S <75538080+Rocky2002-xd@users.noreply.github.com>
2022-07-18 14:56:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function getcookie() {
 function setcookie() {
   document.cookie = "email=abc@gmail.com; expires=Thu, 18 Dec 2022 12:00:00 UTC";
 }
+function clipboard(){
+    navigator.clipboard.readText()
+    .then(text => {
+      console.log('Pasted content: ', text);
+  })
+    .catch(err => {
+      console.error('Failed to read clipboard contents: ', err);
+    });
+  
+}
